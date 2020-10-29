c793e265da779a7a12d027a6fdccb483ffb88428
Katsuki Sato <k-sato@Katsukis-MacBook-Pro-2.local>
2020-10-29 17:06:56 +0900

    Remove unecesarry stuff

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -7,8 +7,6 @@ const firebaseApp: any = firebase.initializeApp({
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
 })
 
 const db = firebaseApp.firestore()
